8b2ab39cf905b8a3164fa8bb6decc011153d7f3e
Agrarii <105797523+Agrarii@users.noreply.github.com>
2023-05-02 20:47:36 +0300

    Update login_page.py
    final update login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -18,3 +18,13 @@ class LoginPage(BasePage):
         assert self.is_element_present(*LoginPageLocators.NEW_EMAIL_LINK), "Email link is not presented"
         assert self.is_element_present(*LoginPageLocators.NEW_PASSWORD_LINK), "Password link is not presented"
         assert self.is_element_present(*LoginPageLocators.CONFIRM_PASSWORD_LINK), "Confirm password link is not presented"
+
+    def register_new_user(self, email, password):
+        email_form = self.browser.find_element(*LoginPageLocators.NEW_EMAIL_LINK)
+        email_form.send_keys(email)
+        new_password = self.browser.find_element(*LoginPageLocators.NEW_PASSWORD_LINK)
+        new_password.send_keys(password)
+        confirm_password = self.browser.find_element(*LoginPageLocators.CONFIRM_PASSWORD_LINK)
+        confirm_password.send_keys(password)
+        button = self.browser.find_element(*LoginPageLocators.REGISTRATION_BTN)
+        button.click()
