e0b18d41df322cdfc6122ed37098a2d9e9240f4a
Agrarii <105797523+Agrarii@users.noreply.github.com>
2023-05-02 20:49:47 +0300

    Add files via upload
    add pytest.ini and final test_product_page.py

---
+++ test_product_page.py
@@ -0,0 +1,77 @@
+from .pages.base_page import BasePage
+from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
+import pytest
+import time
+
+#@pytest.mark.parametrize('promo', [0, 1, 2, 3, 4, 5, 6,
+#                                  pytest.param(7, marks=pytest.mark.xfail),
+#                                  8, 9])
+
+@pytest.mark.need_review
+def test_guest_can_add_product_to_cart(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    #link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_to_basket_btn()
+    page.add_item_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_item_in_basket()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/" 
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_success_message()
+    basket_page.is_basket_empty()
+
+@pytest.mark.user
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def test_registr(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/accounts/login/" 
+        login_page = LoginPage(browser, link)
+        login_page.open()
+        email = str(time.time()) + "@fakemail.org"
+        password = 'wewdwda23456789'
+        login_page.register_new_user(email, password)
+        link2 = "http://selenium1py.pythonanywhere.com/ru/"
+        base_page = BasePage(browser, link2)
+        base_page.open()
+        base_page.should_be_authorized_user  
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/basket/"
+        page = BasketPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+        page.is_basket_empty()
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_cart(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_add_to_basket_btn()
+        page.add_item_to_basket()
+        #page.solve_quiz_and_get_code()
+        page.should_item_in_basket()
+    
