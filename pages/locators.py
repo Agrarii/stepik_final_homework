cf38a5fe37ca34598c7fc70e85540bc752a69810
Agrarii <105797523+Agrarii@users.noreply.github.com>
2023-05-02 20:46:42 +0300

    Update locators.py
    final update locators

--- pages/locators.py
+++ pages/locators.py
@@ -1,7 +1,10 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_MAIN_BTN = (By.CSS_SELECTOR, ".btn-group .btn.btn-default")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class LoginPageLocators():
     EMAIL_LINK = (By.NAME, "login-username")
@@ -9,3 +12,15 @@ class LoginPageLocators():
     NEW_EMAIL_LINK = (By.NAME, "registration-email")
     NEW_PASSWORD_LINK = (By.NAME, "registration-password1")
     CONFIRM_PASSWORD_LINK = (By.NAME, "registration-password2")
+    REGISTRATION_BTN = (By.NAME, "registration_submit")
+
+class ProductPageLocators(object):
+    BASKET_TOTAL = (By.CSS_SELECTOR, ".alertinner p strong")
+    BASKET_STRONG_NAMES = (By.CSS_SELECTOR, ".alertinner strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    BASKET_ADD_BTN = (By.CLASS_NAME, "btn-add-to-basket")
+
+class BasketPageLocators(object):
+    BASKET_EMPTY = (By.CSS_SELECTOR, "#content_inner>p")
+    BASKET_GOODS = (By.CSS_SELECTOR, ".thumbnail")
